df89d6ddef265047541bcfd3bfdec765759f3186
bestehle <bestehle@htwg-konstanz.de>
2015-01-10 00:16:14 +0100

    inject in Main

--- src/main/scala/de/htwg/mps/portals/Main.scala
+++ src/main/scala/de/htwg/mps/portals/Main.scala
@@ -3,13 +3,21 @@ package de.htwg.mps.portals
 import de.htwg.mps.portals.controller.Controller
 import de.htwg.mps.portals.model.Playground
 import de.htwg.mps.portals.view._
+import com.escalatesoft.subcut.inject.AutoInjectable
+import de.htwg.mps.portals.config.TestConfiguration
+import de.htwg.mps.portals.controller.IController
 
-object Portals {
+object Portals extends AutoInjectable {
+  implicit val bindingModule = TestConfiguration
+  // implicit val bindingModule = TuiConfiguration
+  // implicit val bindingModule = GuiConfiguration
+  
   def main(args: Array[String]) {
     val playground = new Playground()
-    val controller = new Controller()
-    val gui = new Gui(controller)
-    val tui = new Tui(controller)
+    val controller = inject[IController]
+    //    val ui = inject [UserInterface]
+    val tui = inject[UserInterface]("tui")
+    val gui = inject[UserInterface]("gui")
     controller.load()
   }
 }
